d0a24dbd96cfc701a40ab817d9070b55ea09384e
Channing Conger <cconger@gmail.com>
2010-03-19 02:49:15 -0400

    Cosmetic Changes to /counter and bug fixes in paths

--- Views/js/progressBarHandler.js
+++ Views/js/progressBarHandler.js
@@ -43,8 +43,8 @@
 			animate		: true,										// Animate the progress? - default: true
 			showText	: false,										// show text with percentage in next to the progressbar? - default : true
 			width		: 120,										// Width of the progressbar - don't forget to adjust your image too!!!
-			boxImage	: 'images/progress/percentImage.png',			// boxImage : image around the progress bar
-			barImage	: 'images/progress/percentImage_back.png',	// Image to use in the progressbar. Can be an array of images too.
+			boxImage	: '/images/progress/percentImage.png',			// boxImage : image around the progress bar
+			barImage	: '/images/progress/percentImage_back.png',	// Image to use in the progressbar. Can be an array of images too.
 			height		: 12,										// Height of the progressbar - don't forget to adjust your image too!!!
 			onTick		: function(pbObj) { return true }
 		}
